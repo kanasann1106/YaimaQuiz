495bdc0587a633a69ad3d56dc4664439ac124911
jang <dragoalie@gmail.com>
2019-02-03 19:01:49 +0900

    Hello Ino World

---
+++ resources/views/ino.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>い の し し 🐗</h1>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ino', 'InoController@index');
+
