f603a5e9e49596d422ba546c1b50b9337df4a3d8
jang <dragoalie@gmail.com>
2019-02-11 17:36:48 +0900

    Migrate kana's view into a framework

---
+++ public/js/quizView.js
@@ -0,0 +1,40 @@
+var questionNum = 1;
+var nextCounter = 0;
+var quiz = new Vue({
+	el: '#quiz',
+	data: {
+		question:{id: 1, sentence: '与那国の方言で「ありがとう」はなんという？'},
+		choices: [
+			{message: 'ふがらっさ'},
+			{message: 'かむさー'},
+			{message: 'てんきゅー'}
+		],
+		btnname:'次へ',
+		showQuestion: true,
+		showExplain: false,
+		showResult: false,
+	},
+	methods: {
+			start: function(){
+				window.location.href = 'quiz'; 
+			},
+			showAnswer: function(){
+				this.showQuestion = !this.showQuestion //false
+				this.showExplain = !this.showExplain //true
+			},
+			next: function(){
+				nextCounter++
+				console.log(nextCounter)
+				if(nextCounter < questionNum){
+					this.showQuestion = !this.showQuestion //true
+					this.showExplain = !this.showExplain //false
+					btnname = '結果へ'
+				}else{
+					this.showResult = !this.showResult //true
+					this.showExplain //ture
+
+				}
+
+			},
+		}
+})
---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	// ハンバーガーメニュー
+	$('.js-toggle-nav').on('click', function(e){
+		$('.top-nav').toggleClass('show');
+		$(this).toggleClass('active');
+	});
+	// ハンバーガーとメニュー以外をクリックしたらナビメニューを閉じる
+	$(document).on('click', function(e){
+		if(!$(e.target).closest('.js-toggle-nav, .top-nav ul').length){
+			$('.top-nav').removeClass('show');
+			$('.js-toggle-nav').removeClass('active');
+		}
+	});
+
+});
