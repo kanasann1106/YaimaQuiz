f16160d3165dae840dcc6b4444a382256fabd444
Haebaru Kana <kana.sakura1106@gmail.com>
2019-02-21 21:21:38 +0900

    Combine quiz and explain

--- yaeyama-quiz/src/components/quiz.vue
+++ yaeyama-quiz/src/components/quiz.vue
@@ -3,7 +3,7 @@
 	<article id="question">
 		<section>
 			<h1>問題 {{quizNum}}. {{quizData[quizNum-1].q}}</h1>
-			<div v-if="show">
+			<div v-if="showQuiz">
 				<div class="img-wrap">
 					<img src="@/assets/uma.jpg">
 				</div>
@@ -16,10 +16,13 @@
 				</div>
 			</div>
 
-			<quiz-explain v-bind:quizData="quizData" v-bind:quizNum="quizNum" v-bind:judgment="judgment" ref="explain" @showQuiz="showQuiz" @endQuiz="alertResult"></quiz-explain>
-
+			<div id="explain" v-if="showExplain">
+				<h2 v-if="judgment"><i class="far fa-circle mr-4"></i>正解！</h2>
+				<h2 v-else><i class="fas fa-times mr-4"></i></i>不正解</h2>
+				<p><strong>解説：</strong>{{quizData[quizNum-1].e}}</p>
+				<button v-on:click="next()" type="button" class="btn btn-primary rounded-pill btn-block">次へ</button>
+			</div>
 		</section>
-
 	</article>
 
 	<quiz-result ref="result" v-bind:totalCorrectNum="totalCorrectNum"></quiz-result>
@@ -29,17 +32,16 @@
 </template>
 
 <script>
-import quizExplain from '@/components/quizExplain'
 import quizResult from '@/components/quizResult'
 export default{
 	name: 'quiz',
 	components:{
-		quizExplain,
 		quizResult
 	},
 	data: function(){
 		return{
 			quizNum: 1,
+			totalQuizNum: 2,
 			quizData: [
 				{q: '与那国島から台湾までの距離はどのくらいでしょう？', a: ['111km','222km','333km'], e: '台湾までの距離は111kmです。たまーに西崎灯台から台湾が見えます！ちなみに石垣島との距離は127kmです。台湾の方が近い！！(笑)'},
 				{q: '与那国島の山の中には不思議な岩がありますがどんな岩でしょう？', a: ['人面岩','光岩','浮遊岩'], e: '人面岩は島の南側の「新川鼻」という岬の高台にあります。獣道を進んでいくと目の前に人面岩が現れます！ちょっとした冒険みたいで楽しいですよ〜♪'},
@@ -47,37 +49,34 @@ export default{
 				{q: '小学校は何校あるでしょう？', a: ['3校','6校','9校'], e: '小学校は3校です。与那国小学校と久部良小学校と比川小学校があります。'},
 				{q: '与那国島の人口はおおよそ何人？', a: ['1700人','540人','15000人'], e: '2018/07/31現在の人口は1680人です。外出先で知り合いに遭遇する確率100%(笑)'},
 			],
-			show: true,
+			showQuiz: true,
+			showExplain: false,
 			judgment: '',
 			totalCorrectNum: 0
 		}
 	},
 	methods: {
 		showAnswer: function(index){
+			this.showQuiz = !this.showQuiz //false
+			this.showExplain = !this.showExplain //true
+
 			if(index === 0){
 				this.judgment = true
 				this.totalCorrectNum++
-				console.log('正解数' + this.totalCorrectNum);
-
-				this.$refs.judgment
 				this.$refs.totalCorrectNum
 			}else{
 				this.judgment = false
 			}
-			this.show = !this.show //false
-			this.$refs.explain.showExplain(this.show)
 		},
-		showQuiz: function(tmp){
-			if(!tmp){
-				this.show = true
+		next: function(){
+			if(this.quizNum < this.totalQuizNum){
+				this.showQuiz = true
+				this.showExplain = false
 				this.quizNum++
+				this.nextCounter++
 			}else{
-				this.show = false
+				this.$refs.result.showResult()
 			}
-
-		},
-		alertResult: function(){
-			this.$refs.result.showResult()
 		},
 	}
 }
@@ -127,5 +126,29 @@ export default{
 	#answer-choices li{
 		font-size: 16px;
 	}
+}
+/*------------------------
+	explain
+------------------------*/
+#explain{
+	border: 4px solid #80cee0;
+	border-radius: 8px;
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	margin-top: 32px;
+	padding: 16px;
+}
+#explain h2{
+	text-align: center;
+	margin-bottom: 16px;
+	font-weight: bold;
+}
+#explain p{
+	font-size: 20px;
+}
+#explain button{
+	width: 56%;
+	height: 56px;
+	margin: 16px auto 0 auto;
 }
 </style>
