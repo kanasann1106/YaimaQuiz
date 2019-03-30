09264134991d02e48c1021381ebbe5408dacee40
Haebaru Kana <kana.sakura1106@gmail.com>
2019-03-30 23:56:13 +0900

    WIP transition vue files

---
+++ resources/js/components/Quiz.vue
@@ -0,0 +1,19 @@
+<template>
+<div>{{ test }}}</div>
+</template>
+
+<script>
+    export default {
+        name: "quiz.vue",
+        props: {
+            test: String,
+        },
+        mounted() {
+            console.log('quiz.vue mounted')
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
