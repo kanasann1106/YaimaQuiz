05ed3f2083124f6310f1372e5d21871ed266c2bf
Haebaru Kana <kana.sakura1106@gmail.com>
2019-03-31 14:16:15 +0900

    Display quiz view

---
+++ resources/js/components/quizResult.vue
@@ -0,0 +1,45 @@
+<template>
+    <article id="result" v-if="show">
+        <section>
+            <h1>クイズ結果</h1>
+            <p v-if="totalCorrectNum > 0">{{totalCorrectNum}}問正解！</p>
+            <p v-else>残念。。もう１度挑戦してみましょう！</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 offset-md-1">
+                        <button v-on:click="start" type="button" class="btn btn-primary rounded-pill btn-block">リトライ</button>
+                    </div>
+                    <div class="col-md-5">
+                        <button type="button" class="btn btn-primary rounded-pill btn-block"><i class="fab fa-twitter fa-lg mr-3"></i>ツイート</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </article>
+</template>
+
+<script>
+    export default{
+        name: 'quizResult',
+        props:['totalCorrectNum'],
+        data: function(){
+            return{
+                show: false
+            }
+        },
+        methods: {
+            showResult: function(){
+                this.show = !this.show //true
+                // this.$emit('show', this.show)
+            },
+            start: function(){
+                window.location.href = '/';
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -11,9 +11,10 @@
 		<meta property="og:url" content="">
 		<meta property="og:image" content="">
 		<meta name="twitter:card" content="summary_large_image">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 
 		<title>クイズ</title>
@@ -78,51 +79,57 @@
 		</header>
 
 		<!-- main -->
-		<main id="quiz" class="cantainer">
-			<article id="question">
-				<section>
-					@foreach($quizzes as $one_quiz)
-					<h1>問題 {{ $one_quiz -> title }}</h1>
-					@endforeach
-					<div v-if="showQuestion">
-						<div class="img-wrap">
-							<img src="{{ asset('image/uma.jpg')}}">
-						</div>
-						<div id="answer-choices">
-							<ul>
-								<li v-for="choice in choices" @click="showAnswer()">
-									@{{ choice.message }}
-								</li>
-							</ul>
-						</div>
-					</div>
+		<div id="app">
+			<quiz/>
+		</div>
+		{{--<main id="quiz" class="cantainer">--}}
+			{{--<article id="question">--}}
+				{{--<section>--}}
+					{{--<div id="app">--}}
+						{{--<quiz test = "kanakana"></quiz>--}}
+					{{--</div>--}}
+					{{--@foreach($quizzes as $one_quiz)--}}
+					{{--<h1>問題 {{ $one_quiz -> title }}</h1>--}}
+					{{--@endforeach--}}
+					{{--<div v-if="showQuestion">--}}
+						{{--<div class="img-wrap">--}}
+							{{--<img src="{{ asset('image/uma.jpg')}}">--}}
+						{{--</div>--}}
+						{{--<div id="answer-choices">--}}
+							{{--<ul>--}}
+								{{--<li v-for="choice in choices" @click="showAnswer()">--}}
+									{{--@{{ choice.message }}--}}
+								{{--</li>--}}
+							{{--</ul>--}}
+						{{--</div>--}}
+					{{--</div>--}}
 
-					<div id="explain" v-if="showExplain">
-						<h2><i class="far fa-circle mr-4"></i>正解！</h2>
-						<!-- <h2><i class="fas fa-times mr-4"></i></i>不正解</h2> -->
-						<p><strong>解説：</strong>カジキ祭りの醍醐味の一つはなんといってもカジキの丸焼き！体長2m超、重さ100kgものカジキマグロを実質食べ放題で召し上がることができます。中までじっくり蒸し焼きにするため6時間以上もかかるんだとか！！（すごい）</p>
-						<button @click="next()" type="button" class="btn btn-primary rounded-pill btn-block">@{{btnname}}</button>
-					</div>
-				</section>
-			</article>
-			<article id="result" v-if="showResult">
-				<section>
-					<h1>クイズ結果</h1>
-					<p>3問正解！</p>
-					<div class="container">
-						<div class="row">
-							<div class="col-md-5 offset-md-1">
-								<button @click="start()" type="button" class="btn btn-primary rounded-pill btn-block">リトライ</button>
-							</div>
-							<div class="col-md-5">
-								<button type="button" class="btn btn-primary rounded-pill btn-block"><i class="fab fa-twitter fa-lg mr-3"></i>ツイート</button>
-							</div>
-						</div>
-					</div>
-				</section>
-			</article>
+					{{--<div id="explain" v-if="showExplain">--}}
+						{{--<h2><i class="far fa-circle mr-4"></i>正解！</h2>--}}
+						{{--<!-- <h2><i class="fas fa-times mr-4"></i></i>不正解</h2> -->--}}
+						{{--<p><strong>解説：</strong>カジキ祭りの醍醐味の一つはなんといってもカジキの丸焼き！体長2m超、重さ100kgものカジキマグロを実質食べ放題で召し上がることができます。中までじっくり蒸し焼きにするため6時間以上もかかるんだとか！！（すごい）</p>--}}
+						{{--<button @click="next()" type="button" class="btn btn-primary rounded-pill btn-block">@{{btnname}}</button>--}}
+					{{--</div>--}}
+				{{--</section>--}}
+			{{--</article>--}}
+			{{--<article id="result" v-if="showResult">--}}
+				{{--<section>--}}
+					{{--<h1>クイズ結果</h1>--}}
+					{{--<p>3問正解！</p>--}}
+					{{--<div class="container">--}}
+						{{--<div class="row">--}}
+							{{--<div class="col-md-5 offset-md-1">--}}
+								{{--<button @click="start()" type="button" class="btn btn-primary rounded-pill btn-block">リトライ</button>--}}
+							{{--</div>--}}
+							{{--<div class="col-md-5">--}}
+								{{--<button type="button" class="btn btn-primary rounded-pill btn-block"><i class="fab fa-twitter fa-lg mr-3"></i>ツイート</button>--}}
+							{{--</div>--}}
+						{{--</div>--}}
+					{{--</div>--}}
+				{{--</section>--}}
+			{{--</article>--}}
 
-		</main>
+		{{--</main>--}}
 
 		<!-- footer -->
 		<footer id="footer">
@@ -134,6 +141,7 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+		<script src=" {{ mix('js/app.js') }} "></script>
 		<script type="text/javascript" src="{{ asset('js/footerFixed.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('js/quizView.js') }}"></script>
