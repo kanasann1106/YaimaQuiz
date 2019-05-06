378d5ac07b964a32397c4e294fadd3c0ae8b7d74
Haebaru Kana <kana.sakura1106@gmail.com>
2019-05-06 14:55:48 +0900

    Deploy Local 🎉

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://yaima-quiz.kanasann.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,8 +9,8 @@
   <meta property="og:type" content="website" />
   <meta property="og:title" content="やいまクイズ">
   <meta property="og:description" content="沖縄の南西に位置する八重山諸島(石垣島、西表島、竹富島、与那国島など)に関するクイズを楽しめるサービスです。訪れるだけではわからない島の魅力が発見できるかも！！">
-  <meta property="og:url" content="https://yaeyama.kanasann.com">
-  <meta property="og:image" content="images/thumnail.png">
+  <meta property="og:url" content="https://yaima-quiz.kanasann.com">
+  <meta property="og:image" content="https://kanasann.com/images/yaima_img.png">
   <meta name="twitter:card" content="summary_large_image">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
