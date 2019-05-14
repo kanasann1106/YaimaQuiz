29b5a2f9e51907b1fc13266ea484602f0c730546
Haebaru Kana <kana.sakura1106@gmail.com>
2019-05-15 00:51:25 +0900

    クイズ一覧レイアウト、削除、編集ボタン修正

--- resources/views/userpage/quiz_posts.blade.php
+++ resources/views/userpage/quiz_posts.blade.php
@@ -9,23 +9,38 @@
     <table class="table table-striped">
       <thead>
         <tr>
-          <th>{{ __('NO.') }}</th>
-          <th>{{ __('Title') }}</th>
-          <th>{{ __('Category') }}</th>
-          <th>{{ __('地域') }}</th>
-          <th>{{ __('updated_at') }}</th>
+          <th style="width:16px">{{ __('NO.') }}</th>
+          <th style="width:400px">{{ __('Title') }}</th>
+          <th style="width:120px">{{ __('Category') }}</th>
+          <th style="width:88px">{{ __('地域') }}</th>
+          <th style="width:110px">{{ __('updated_at') }}</th>
+          <th style="width:80px">{{ __('編集') }}</th>
+          <th style="width:80px">{{ __('削除') }}</th>
+
         </tr>
       </thead>
       <tbody>
         @foreach ($quiz_posts as $key => $quiz)
         <tr>
           <td>{{ $key+1 }}</td>
+          <td>{{ $quiz->title }}</td>
+          <td>{{ $quiz->category->name }}</td>
+          <td>{{ $quiz->region->name }}</td>
+          <td>{{ $quiz->updated_at->format('Y/m/d') }}</td>
           <td>
-            <a href="{{ url('quiz_posts/'.$quiz->id) }}">{{ $quiz->title }}</a>
+            <a href="{{ url('mypage/'.$quiz->id.'/edit') }}" class="btn btn-primary">
+              <i class="fas fa-pencil-alt"></i>
+            </a>
           </td>
-          <td>{{ $quiz->user.email }}</td>
-          <td>{{ $quiz->region }}</td>
-          <td>{{ $quiz->updated_at }}</td>
+          <td>
+            <form style="display:inline" action="{{ url('mypage/'.$quiz->id) }}" method="post">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-danger">
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </form>
+          </td>
         </tr>
         @endforeach
       </tbody>
