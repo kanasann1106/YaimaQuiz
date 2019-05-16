4d596a65fa50769718e8de888657a21ff55334cb
Haebaru Kana <kana.sakura1106@gmail.com>
2019-05-16 23:11:56 +0900

    クイズ一覧ページデザイン修正

--- resources/views/userpage/quiz_posts.blade.php
+++ resources/views/userpage/quiz_posts.blade.php
@@ -3,17 +3,15 @@
 @section('title','クイズ一覧')
 
 @section('content')
-<div class="container">
+
+<div class="container mypage-quizlist">
   <h1>@yield('title')</h1>
   <div class="table-responsive">
     <table class="table table-striped">
       <thead>
         <tr>
           <th style="width:16px">{{ __('NO.') }}</th>
-          <th style="width:400px">{{ __('Title') }}</th>
-          <th style="width:120px">{{ __('Category') }}</th>
-          <th style="width:88px">{{ __('地域') }}</th>
-          <th style="width:110px">{{ __('updated_at') }}</th>
+          <th style="width:400px">{{ __('タイトル') }}</th>
           <th style="width:80px">{{ __('編集') }}</th>
           <th style="width:80px">{{ __('削除') }}</th>
 
@@ -24,11 +22,9 @@
         <tr>
           <td>{{ $key+1 }}</td>
           <td>{{ $quiz->title }}</td>
-          <td>{{ $quiz->category->name }}</td>
-          <td>{{ $quiz->region->name }}</td>
-          <td>{{ $quiz->updated_at->format('Y/m/d') }}</td>
+          <!-- <td></td> -->
           <td>
-            <a href="{{ url('mypage/'.$quiz->id.'/edit') }}" class="btn btn-primary">
+            <a href="{{ url('mypage/'.$quiz->id.'/edit') }}" class="btn btn-edit">
               <i class="fas fa-pencil-alt"></i>
             </a>
           </td>
