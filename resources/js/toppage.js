29bad9fef96a4781ce5c37f0fc632bd32e35a81c
Haebaru Kana <kana.sakura1106@gmail.com>
2019-06-03 23:01:30 +0900

    Minify対応

---
+++ resources/js/toppage.js
@@ -0,0 +1,6 @@
+$(function () {
+  //TOPページ画像スライドショー
+  $('.js-slider').bgswitcher({
+    images: ['images/back03.jpg', 'images/back02.jpg', 'images/back04.jpg', 'images/back05.jpg', 'images/back01.jpg']
+  });
+});
