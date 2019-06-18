9f9e7d5d2ca6c90bdb873243b8a59b7877e76273
Haebaru Kana <kana.sakura1106@gmail.com>
2019-06-19 00:11:52 +0900

    インデント修正

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $categories = ['食べ物', '生物', '文化・歴史', '雑学'];
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$categories = ['食べ物', '生物', '文化・歴史', '雑学'];
 
-        foreach ($categories as $category) {
-            DB::table('categories')->insert([
-                'name' => $category,
-            ]);
-        }
-    }
+		foreach ($categories as $category) {
+			DB::table('categories')->insert([
+				'name' => $category,
+			]);
+		}
+	}
 }
